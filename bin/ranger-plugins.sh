a6b3029e63aa5f6f6df847532e785cc9d0d80e59
norrishuang <12380647@qq.com>
2024-10-16 20:48:59 +0800

    fix issue

--- bin/ranger-plugins.sh
+++ bin/ranger-plugins.sh
@@ -42,8 +42,13 @@ testEmrSshConnectivity() {
             systemctl --type=service --state=running|egrep '^(hadoop|hbase|hive|spark|hue|presto|oozie|zookeeper|flink)\S*'
 EOF
             if [ ! "$?" = "0" ]; then
-                echo "ERROR!! connection to [ $masterNode ] failed!"
-                exit 1
+                ssh -o ConnectTimeout=5 -o ConnectionAttempts=1 -o StrictHostKeyChecking=no -i $SSH_KEY -T hadoop@$masterNode <<EOF
+                            systemctl --type=service --state=running|egrep '(hadoop|hbase|hive|spark|hue|presto|oozie|zookeeper|flink)\S*'
+EOF
+                if [ ! "$?" = "0" ]; then
+                  echo "ERROR!! connection to [ $masterNode ] failed!"
+                  exit 1
+                fi
             fi
         done
         for node in $(getEmrSlaveNodes); do
